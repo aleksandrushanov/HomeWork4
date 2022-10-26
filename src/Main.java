669bced24a5b84fd89742d65f8e5d5f4aded0bbd
Саша <kresh_sasha@icloud.com>
2022-10-26 20:52:12 +0700

    Task 1.2.3

--- src/Main.java
+++ src/Main.java
@@ -1,41 +1,38 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Home Work 4");
+        System.out.println("Домашнее задание 2");
         System.out.println("Task 1 ");
-       int age =18;
-       if ( age >=18) {
-           System.out.println("Поздровляем пользователя с совершеннолетием");
-       }
-       if (age <18) {
-            System.out.println("возраст совершеннолетия ещё не наступил");
-        }
-        System.out.println("Task2");
-
-        int ageChild = 224;
-        if (ageChild >=7 && ageChild <18) {
-        System.out.println("ребенок ходит в школу");}
-
-
-        if (ageChild >=18 && ageChild < 24) {
+      int age= 15;
+      if (age >=18) {
+          System.out.println("поздравляем пользователя с совершеннолетием");
+      }
+      else {
+          System.out.println( "возраст совершеннолетия ещё не наступил, и нужно немного подождать.");
+      }
+        System.out.println("Task 2");
+        int childAge=21;
+        if (childAge>=7 && childAge <18){
+            System.out.println("ребенок ходит в школу");
+        } else if (childAge >=18 && childAge <24)
+        {
             System.out.println("человек уже закончил школу и может отправляться в университет");
-        }
-        if (ageChild >= 24) {
+        }else{
             System.out.println("человек окончил университет и ему пора искать первую работу");
         }
-
         System.out.println("Task3");
 
-        int carCapacity = 104;
-        if (carCapacity > 102) {
-            System.out.println("Мест нету");
-        }
-        if (carCapacity <=60) {
+        int carCapacity = 130;
+        if (carCapacity <=60){
             System.out.println("Есть сидячие места");
         }
-        if (carCapacity >= 60 && carCapacity <102) {
-            System.out.println("Оставлись только стоячие места");
+        else if (carCapacity >=60 && carCapacity <102){
+            System.out.println("Осталось место только стоя");
         }
-            System.out.println("Task4");
+        else {
+            System.out.println("Мест нету");
+        }
+
 
 
     }
